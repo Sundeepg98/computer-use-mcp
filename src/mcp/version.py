7ea81aef232420cf326c58bd4a049cc82a1f8099
sundeepg98 <sundeepg8@gmail.com>
2025-08-04 15:41:35 +0000

    Update author attribution to use full name 'Sundeep G'

--- src/mcp/version.py
+++ src/mcp/version.py
@@ -2,7 +2,7 @@
 
 __version__ = "1.0.0"
 __version_info__ = (1, 0, 0)
-__author__ = "sundeepg98"
+__author__ = "Sundeep G"
 __author_email__ = "sundeepg8@gmail.com"
 __license__ = "MIT"
-__copyright__ = "Copyright 2025 sundeepg98"
+__copyright__ = "Copyright 2025 Sundeep G"
